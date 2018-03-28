dcf61754cc5d4f43532f645736126a692c8f1a07
mathieu <coolbreeze@localhost.localdomain>
2018-03-28 15:20:17 -0400

    Mathieu:commencement base de donnee

--- programme/p4.js
+++ programme/p4.js
@@ -1,5 +1,7 @@
 var amqp = require('amqplib/callback_api');
-
+var message1;
+var message2;
+var Client = require('mariasql');
 amqp.connect('amqp://localhost', function(err, conn) {
   conn.createChannel(function(err, ch) {
     var q = 'textTraduit';
@@ -10,9 +12,20 @@ ch.consume(q, function(msg) {
 
 
 //ICI ON RECOIT LE MESSAGE DE P2
-var message = msg.content.toString();
-message = message.split('/');
-  console.log(message);
+message1 = msg.content.toString();
+message1 = message1.split('/');
+  console.log(message1);
+var c = new Client({
+  		host: 'localhost',
+  		user: 'root',
+  		password: 'test',
+		db: 'testMariadbGalera'
+	});
+	c.query("INSERT INTO texte (En, Fr) VALUES ('"+message1[0]+"','"+message1[1]+"')", function (err, result) {
+if(err)
+console.log(err);
+	});
+
 }, {noAck: true});
   });
 });
@@ -28,10 +41,46 @@ ch.consume(q, function(msg) {
 
 
 //ICI ON RECOIT LE MESSAGE DE P3
-var message = msg.content.toString();
-message = message.split('/');
-  console.log(message);
+message2 = msg.content.toString();
+message2 = message2.split('/');
+  console.log(message2);
+fs.open(message2[0], 'r', function (status, fd) {
+    if (status) {
+        console.log(status.message);
+        return;
+    }
+    var fileSize = getFilesizeInBytes(message2[0]);
+    var buffer = new Buffer(fileSize);
+    fs.read(fd, buffer, 0, fileSize, 0, function (err, num) {
+
+        var query = "INSERT INTO image SET ?",
+            values = {
+                file_type: 'img',
+                file_size: buffer.length,
+                file: buffer
+            };
+
+var c = new Client({
+  		host: 'localhost',
+  		user: 'root',
+  		password: 'test',
+		db: 'testMariadbGalera'
+	});
+        	c.query("INSERT INTO texte (En, Fr) VALUES ('"+message1[0]+"','"+message1[1]+"')", function (err, result) {
+if(err)
+console.log(err);
+	});
+
+    });
+});
 }, {noAck: true});
   });
+
 });
+
+
+
+
+
+
 
